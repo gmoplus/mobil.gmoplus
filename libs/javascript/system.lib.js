0b5e3a7c35bb010f40ef84698547c008c2c6d066
gmoplus <gmoplusx@gmail.com>
2026-01-19 12:32:14 +0300

    Fix frontend JS 'readCookie' error and Admin Panel .htaccess rewrite loop

--- libs/javascript/system.lib.js
+++ libs/javascript/system.lib.js
@@ -22,6 +22,34 @@
  *  https://www.flynax.com
  ******************************************************************************/
 
+/*
+ * Flynax Utils
+ */
+var createCookie = function(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+var readCookie = function(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+var eraseCookie = function(name) {
+    createCookie(name, "", -1);
+}
+
 var flynaxClass = function(){
     /**
     * current step by switchStep method
