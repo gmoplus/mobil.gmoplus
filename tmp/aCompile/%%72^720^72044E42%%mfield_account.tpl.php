5a36b1160f974cb264239b4611adb2e5eeb8e8c2
gmoplus <gmoplusx@gmail.com>
2026-01-26 16:47:54 +0300

    chore: secure repo by excluding data and secrets (files, backup, local config)

--- tmp/aCompile/%%72^720^72044E42%%mfield_account.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.31, created on 2025-07-13 01:53:26
-         compiled from /home/gmoplus/mobil.gmoplus.com/plugins/multiField/mfield_account.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('modifier', 'cat', '/home/gmoplus/mobil.gmoplus.com/plugins/multiField/mfield_account.tpl', 1, false),)), $this); ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => ((is_array($_tmp=((is_array($_tmp=((is_array($_tmp=(defined('RL_PLUGINS') ? @RL_PLUGINS : null))) ? $this->_run_mod_handler('cat', true, $_tmp, 'multiField') : smarty_modifier_cat($_tmp, 'multiField')))) ? $this->_run_mod_handler('cat', true, $_tmp, (defined('RL_DS') ? @RL_DS : null)) : smarty_modifier_cat($_tmp, (defined('RL_DS') ? @RL_DS : null))))) ? $this->_run_mod_handler('cat', true, $_tmp, 'mfield.tpl') : smarty_modifier_cat($_tmp, 'mfield.tpl')), 'smarty_include_vars' => array('data_mode' => 'account')));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
--- tmp/aCompile/%%A9^A98^A981BB55%%apTplFooter.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.31, created on 2025-07-27 12:57:13
-         compiled from /home/gmoplus/mobil.gmoplus.com/plugins/priceHistory/view/apTplFooter.tpl */ ?>
-<script>
-    var ph_listing_id = '<?php echo $_GET['id']; ?>
-';
-    $('#edit_listing > fieldset').first().after('<div id="ph-container"></div>');
-    $('#ph-container').load(rlPlugins + 'priceHistory/admin/price_history_edit.php?id=' + ph_listing_id);
-</script>
